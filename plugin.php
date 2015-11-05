6e77f64e21ef547f3a7be9f695bf347f3bc19c2c
Sam Schmidt <samuel@dersam.net>
2015-11-05 15:31:36 -0500

    Autoload iddqd.

--- plugin.php
+++ plugin.php
@@ -1,8 +1,28 @@
 <?php
 /**
+ * Prophet plugin for GodMode
  *
+ * Provides helpers to allow use of Iddqd in testing context to rewrite
+ * classes and replace them with mocks.
  *
  * @author Sam Schmidt <samuel@dersam.net>
  * @since 2015-11-05
  * @company Linus Shops
  */
+
+require __DIR__.'/vendor/autoload.php';
+
+$dir = __DIR__;
+
+$loader = function($classname) use($dir) {
+    $path = explode('_', $classname);
+    include $dir
+        .'/vendor/linusshops/iddqd/src/app/code/community/'
+        .implode('/',$path)
+        .'.php'
+    ;
+};
+
+spl_autoload_register($loader);
+
+$config = new Linus_Iddqd_Model_Config();
