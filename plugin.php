9266a9305b4efbd12f7096834156650f5b61ffed
Sam Schmidt <samuel@dersam.net>
2015-11-05 17:04:52 -0500

    Add config option to use installed iddqd.

--- plugin.php
+++ plugin.php
@@ -13,28 +13,34 @@
 namespace LinusShops\Prophet\Plugins;
 
 use LinusShops\Prophet\Events;
-use LinusShops\Prophet\Events\Options;
 use LinusShops\Prophet\Plugin;
 use LinusShops\Prophet\PluginRepository;
 use PD;
 
 class Iddqd implements Plugin
 {
+    private $config = array();
+
     public function load()
     {
         require __DIR__ . '/vendor/autoload.php';
 
         $dir = __DIR__;
+        $this->config = json_encode(__DIR__.'/config.json',true);
+        //Use the installed version of godmode, instead of the one bundled with the plugin.
+        $useMagentoGodmode = isset($this->config['use_magento_godmode']) ?
+            $this->config['use_magento_godmode'] : false;
+        ;
 
-        $loader = function ($classname) use ($dir) {
+        $loader = function ($classname) use ($dir, $useMagentoGodmode) {
+            if ($useMagentoGodmode) {
+                return;
+            }
             $path = explode('_', $classname);
-            $path = $dir
+            require $dir
                 . '/vendor/linusshops/iddqd/src/app/code/community/'
                 . implode('/', $path)
                 . '.php';
-            if (file_exists($path)) {
-                require ($path);
-            }
         };
 
         spl_autoload_register($loader);
@@ -42,7 +48,7 @@ class Iddqd implements Plugin
 
     public function register()
     {
-        PD::listen(Events::PROPHET_PREMAGENTO, function(Options $options){
+        PD::listen(Events::PROPHET_PREMAGENTO, function(&$options=array()){
             echo "Initializing GodMode...".PHP_EOL;
             $options->set('config_model', 'Linus_Iddqd_Model_Config');
         });
